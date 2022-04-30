b1c4253d5c5211d268029fd84bd51e743edb8815
permafrost06 <zahin06@gmail.com>
2022-04-30 17:17:42 +0600

    moved to object based parsing created object PTHistory moved previous method to `old_script.py` added gzip reading

---
+++ PTHistory.py
@@ -0,0 +1,128 @@
+from struct import *
+from math import floor
+from typing import BinaryIO
+
+def readBytes(formatstr: str, fp: BinaryIO):
+    return unpack(formatstr, fp.read(calcsize(formatstr)))
+
+class Tag:
+    def __init__(self, name, color):
+        self.name = name
+        self.color = color
+
+    def __repr__(self) -> str:
+        return f"<Tag object '{self.name}'>"
+
+    def __str__(self) -> str:
+        return f"Tag: {self.name} - {self.color}"
+
+class Day:
+    def __init__(self, fp: BinaryIO):
+        (self.day, self.firstminuteused,
+        self.activeseconds, self.semiidleseconds,
+        self.key, self.lmb, self.rmb, self.scrollwheel
+        ) = readBytes("HHiiiiii", fp)
+
+    def __repr__(self) -> str:
+        return f"<Day object {self.day}>"
+    
+class Node:
+    def __init__(self):
+        self.name: str = ""
+        self.tagindex: int = 0
+        self.ishidden: str = ""
+        self.numberofdays: int = 0
+
+        self.days: list[Day] = []
+
+        self.numchildren: int = 0
+
+        self.children: list['Node'] = []
+
+    def __repr__(self) -> str:
+        return f"<Node object '{self.name}'>"
+
+    def __readName(self, fp: BinaryIO) -> str:
+            name = b''    # 	char *name (null terminated bytes
+            # do
+            lastchar = fp.read(1)
+            name += lastchar
+            while lastchar != b'\x00':
+                lastchar = fp.read(1)
+                name += lastchar
+            
+            return name
+
+    def createRoot(self, fp: BinaryIO):
+        self.name = self.__readName(fp)
+        (self.tagindex,) = readBytes("i", fp)
+        (self.ishidden,) = readBytes("c", fp)
+        (self.numberofdays,) = readBytes("i", fp)
+
+        for i in range(self.numberofdays):
+            self.days.append(Day(fp))
+        
+        (self.numchildren,) = readBytes("i", fp)
+
+        for i in range(self.numchildren):
+            childNode = Node()
+            childNode.getChild(fp)
+            self.children.append(childNode)
+
+    def getChild(self, fp: BinaryIO):
+        name = self.__readName(fp)
+        name = name.rstrip(b'\0')
+        name = name.decode()
+        self.name = name
+
+        (self.tagindex,) = readBytes("i", fp)
+        (self.ishidden,) = readBytes("c", fp)
+        (self.numberofdays,) = readBytes("i", fp)
+
+        for i in range(self.numberofdays):
+            self.days.append(Day(fp))
+        
+        (self.numchildren,) = readBytes("i", fp)
+
+        for i in range(self.numchildren):
+            childNode = Node()
+            childNode.getChild(fp)
+            self.children.append(childNode)
+
+class PTHistory:
+    def __init__(self):
+        self.version: int = 0
+        self.magic: int = 0
+        self.numtags: int = 0
+
+        self.tags: list[Tag] = []
+    
+        self.minfilter: int = 0
+        self.foldlevel: int = 0
+
+        self.perfs: list[int] = []
+
+        self.root: Node = Node()
+
+    def parseHistory(self, fp: BinaryIO):
+        (self.version,) = readBytes("i", fp)
+        if not self.version == 13:
+            raise ValueError("File version is not supported")
+
+        self.magic = pack("i", readBytes(">i", fp)[0])
+        if not self.magic == b'PTFF':
+            raise ValueError("File is not ProcrastiTracker database file")
+
+        (self.numtags,) = readBytes("i", fp)
+
+        for i in range(self.numtags):
+            (name, color) = readBytes("<32si", fp)
+            name = name.rstrip(b'\0')
+            name = name.decode()
+            self.tags.append(Tag(name, color))
+        
+        (self.minfilter, self.foldlevel) = readBytes("ii", fp)
+
+        self.perfs = list(readBytes("10i", fp))
+
+        self.root.createRoot(fp)
--- script.py
+++ script.py
@@ -1,101 +1,13 @@
-from struct import *
-from math import floor
+from PTHistory import PTHistory
+import gzip
 
-def readBytes(formatstr):
-    unpacked_tuple = unpack(formatstr, x.read(calcsize(formatstr)))
-    if len(formatstr) == 1:
-        return unpacked_tuple[0]
-    else:
-        return unpacked_tuple
+x = PTHistory()
 
-def getHHMMSS(seconds):
-    timestr = ""
-    HH = floor(seconds/(60*60))
-    if HH > 0:
-        timestr += f"{HH:02d}:"
-    else:
-        timestr += "00:"
-    MM = floor(seconds/60)%60
-    if MM > 0:
-        timestr += f"{MM:02d}:"
-    else:
-        timestr += "00:"
-    SS = seconds%60
-    if SS:
-        timestr += f"{SS:02d}"
-    else:
-        timestr += "00"
+fp = gzip.open("data/exported_view 20220101.PT", "rb")   # data from 01/01/2022
 
-    return timestr
-
-def dayToDate(day_int):
-    bin = pack(">H", day_int)
-    hexstr = bin.hex()
-    binarystr = format(int(hexstr, 16), "16b")
-    datestr = f"{int(binarystr[-5:], 2)}/{int(binarystr[-9:-5], 2)}/20{int(binarystr[0:-9], 2)}"
-    return datestr
-
-# x = open("data/exported_view all 20220427", "rb")   # all data till 27/04/2022
-# x = open("data/exported_view 20210804", "rb")   # data from 04/08/2021
-# x = open("data/exported_view 20220430", "rb")   # data from 30/04/2022
-x = open("data/exported_view 20220101", "rb")   # data from 01/01/2022
-# x = open("data/exported_view 20210514", "rb") # data from 14/5/2021
-
-# struct DATABASE {
-# 	int version (== 13, as of June 9 2019, format may be different if other version)
-# 	int magic (== 'PTFF' on x86)
-# 	int numtags
-(version, magic, numtags) = unpack("iii", x.read(12))
-print(version, magic, numtags)
-
-# 	struct { char name[32]; int color; } tags[numtags]
-for i in range(numtags):
-    (name, color) = unpack("<32si", x.read(36))
-    print(name, color)
-
-# 	int minfilter
-# 	int foldlevel
-(minfilter, foldlevel) = unpack("ii", x.read(8))
-print(minfilter, foldlevel)
-
-# 	int prefs[10] (see advanced prefs window, also see "prefs" in procrastitracker.cpp)
-perfs = unpack("10i", x.read(40))
-print(perfs)
-
-# 	NODE root
-def getNode():
-
-    # timestruct NODE {
-    name = b''    # 	char *name (null terminated bytes
-    lastchar = x.read(1)
-    name += lastchar
-    while lastchar != b'\x00':
-        lastchar = x.read(1)
-        name += lastchar
-    print(name)
-    tagindex = readBytes("i")    # 	int tagindex
-    ishidden = readBytes("c")    # 	char ishidden
-    numberofdays = readBytes("i")    # 	int numberofdays
-    print(tagindex, ishidden, numberofdays)
-
-    for i in range(numberofdays):    # 	DAY days[numberofdays]
-
-        # timestruct DAY {
-        (day,   # 	unsigned short day
-                #   (lower 5 bits = day, next 4 bits = month, rest = year counted from 2000)
-        firstminuteused,    # 	unsigned short firstminuteused (counted from midnight)
-        # 	int activeseconds, semiidleseconds, key, lmb, rmb, scrollwheel
-        activeseconds, semiidleseconds, key, lmb, rmb, scrollwheel
-        ) = readBytes("HHiiiiii")
-        print(day, firstminuteused, activeseconds, semiidleseconds, key, lmb, rmb, scrollwheel)
-        print(dayToDate(day))
-        print(getHHMMSS(activeseconds))
-        # }
-
-    numchildren = readBytes("i")    # 	int numchildren
-    for i in range(numchildren):    # 	NODE children[numchildren]
-        getNode()
-    # }
-# }
-
-getNode()
+x.parseHistory(fp)
+print(x.version)
+print(x.magic)
+print(x.numtags)
+print(x.tags)
+print(x.root.children[-11].children)
