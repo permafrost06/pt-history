d15ccf5c0360b7c211f204b92ee849f40f1d5186
permafrost06 <zahin06@gmail.com>
2022-05-14 18:04:27 +0600

    save data as json

--- script.py
+++ script.py
@@ -1,13 +1,32 @@
-from PTHistory import PTHistory
+from datetime import timedelta
+from PTHistory import Node, PTHistory
 import gzip
+import json
 
 x = PTHistory()
 
-fp = gzip.open("data/exported_view 20220101.PT", "rb")   # data from 01/01/2022
+with gzip.open("data/exported_view all 20220502.PT", "rb") as fp:   # data from 01/01/2022
 
-x.parseHistory(fp)
-print(x.version)
-print(x.magic)
-print(x.numtags)
-print(x.tags)
-print(x.root.children[-11].children)
+    x.parseHistory(fp)
+
+    # for child in x.root.children:
+    #     print(child)
+
+# obj = x.toDict()
+obj = dict(x)
+
+with open("data/dump.json", "w") as fp:
+    json.dump(obj, fp)
+
+# print(dict(x))
+
+# def getNodeSeconds(node: Node):
+#     seconds = sum(day.activeseconds for day in node.days)
+#     for child in node.children:
+#         seconds += getNodeSeconds(child)
+#     return seconds
+
+# totalseconds = getNodeSeconds(x.root)
+# print(timedelta(seconds=totalseconds))
+
+# print(sorted(x.root.children, key=lambda x: x.totalseconds, reverse=True))
